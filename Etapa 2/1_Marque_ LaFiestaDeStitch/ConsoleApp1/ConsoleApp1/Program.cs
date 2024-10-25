88e72eab135604f9013e2db93ef4a73f7ce5848a
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-25 17:32:50 -0300

    Add files via upload

---
+++ Etapa 2/1_Marque_ LaFiestaDeStitch/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int invitados;
+            Console.WriteLine("cuantos invitados hay?");
+            invitados = int.Parse(Console.ReadLine());
+
+
+            int[] vector = new int[invitados];
+            int comida;
+
+            for (int i = 0; i < invitados; i++)
+            {
+
+                Console.WriteLine("cuanta comida come el invitado " + (i + 1));
+
+                comida = int.Parse(Console.ReadLine());
+
+
+                if (comida > 100)
+                {
+                    Console.WriteLine("error, numero menor de 100");
+                    i--;
+                }
+                else if (comida < 100)
+                {
+                    vector[i] = comida;
+
+                }
+
+            }
+
+            int acumulador = 0;
+
+            for (int i = 0; i < invitados; i++)
+            {
+                acumulador += vector[i];
+            }
+            Console.WriteLine("el promedio final es " + (acumulador / invitados));
+            Console.ReadKey();
+        }
+    }
+}
