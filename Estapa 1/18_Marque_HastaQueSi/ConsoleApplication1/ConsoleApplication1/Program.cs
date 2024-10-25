6248da02712adb3df39fced5a631e5013913a0e5
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-25 16:47:32 -0300

    tp 18

---
+++ Estapa 1/18_Marque_HastaQueSi/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta = " " ;
+            
+            Console.WriteLine("DIGA SI PARA CONTINUAR");
+            
+            while (respuesta != "SI")
+            {
+                Console.WriteLine("¿desea terminar el programa?");
+                respuesta = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("¡Hasta la vista!");
+            Console.ReadKey();
+        }
+    }
+}
