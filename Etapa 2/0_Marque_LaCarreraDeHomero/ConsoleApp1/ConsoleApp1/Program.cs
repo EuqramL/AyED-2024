b591be8344b0f1eebac74d73c283ff294e8eb286
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-25 17:28:42 -0300

    Etapa 2, tp0

---
+++ Etapa 2/0_Marque_LaCarreraDeHomero/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int obstaculos;
+            Console.WriteLine("cuantos obstaculos tiene homero?");
+            obstaculos = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[obstaculos];
+            string respuesta;
+
+            for (int i = 0; i < obstaculos; i++)
+            {
+
+                Console.WriteLine("supera el obstaculo " + i);
+                respuesta = Console.ReadLine();
+
+
+                if (respuesta == "si")
+                {
+                    vector[i] += 10;
+                }
+                else if (respuesta == "no")
+                {
+                    vector[i] -= 5;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    i--;
+                }
+
+
+
+            }
+
+            int acumulador = 0;
+
+            for (int i = 0; i < obstaculos; i++)
+            {
+                acumulador += vector[i];
+            }
+            Console.WriteLine("el punatje final es: " + acumulador);
+            Console.ReadKey();
+        }
+    }
+}
