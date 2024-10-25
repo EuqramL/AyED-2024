d27c1f80964f0a9d53975d1a7f23665c8aff5379
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-25 17:16:22 -0300

    tp17

---
+++ Estapa 1/17_Marque_MiPrimerMenuObjetivo/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            numero = 0;
+
+            Console.WriteLine("Bienvendio al Programa");
+            Console.WriteLine("elija una de las opciones");
+            Console.WriteLine("1- suma de numeros");
+            Console.WriteLine("2- resta de numeros");
+            Console.WriteLine("3- multiplicacion de numeros");
+            Console.WriteLine("4- salir");
+
+            while (numero != 4)
+            {
+                numero = int.Parse(Console.ReadLine());
+                if (numero == 1)
+                {
+                    Console.WriteLine("menu 1: suma de numeros");
+                    Console.WriteLine("elije 2 numeros para sumarlos");
+                    int num1;
+                    num1 = int.Parse(Console.ReadLine());
+
+                    int num2;
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("esta es el resultado " + (num1 + num2));
+
+                    Console.WriteLine("elija una de las opciones de nuevo");
+                    Console.WriteLine("1- suma de numeros");
+                    Console.WriteLine("2- resta de numeros");
+                    Console.WriteLine("3- multiplicacion de numeros");
+                    Console.WriteLine("4- salir");
+
+                }
+                if (numero == 2)
+                {
+                    Console.WriteLine("menu 2: resta de numeros");
+                    Console.WriteLine("elije 2 numeros para restarlos");
+                    int num3;
+                    num3 = int.Parse(Console.ReadLine());
+
+                    int num4;
+                    num4 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("esta es el resultado" + (num3 - num4));
+
+                    Console.WriteLine("elija una de las opciones de nuevo");
+                    Console.WriteLine("1- suma de numeros");
+                    Console.WriteLine("2- resta de numeros");
+                    Console.WriteLine("3- multiplicacion de numeros");
+                    Console.WriteLine("4- salir");
+
+                }
+                if (numero == 3)
+                {
+                    Console.WriteLine("menu 3: multiplicacion de numeros");
+                    Console.WriteLine("elije 2 numeros para multiplicarlos");
+                    int num5;
+                    num5 = int.Parse(Console.ReadLine());
+
+                    int num6;
+                    num6 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("esta es el resultado" + (num5 * num6));
+
+                    Console.WriteLine("elija una de las opciones de nuevo");
+                    Console.WriteLine("1- suma de numeros");
+                    Console.WriteLine("2- resta de numeros");
+                    Console.WriteLine("3- multiplicacion de numeros");
+                    Console.WriteLine("4- salir");
+                    numero = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("hasta luego");
+            Console.ReadKey();
+
+        }
+    }
+
+}
+    
+
