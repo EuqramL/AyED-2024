25986574ebe1e83122b59fba4be0178e6f2ea79d
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-25 17:50:29 -0300

    Add files via upload

---
+++ Etapa 2/6_Marque_ZEGARPG/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            Console.WriteLine("¿cuantos objetos hay?");
+            int[] vector = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                Console.WriteLine("objeto " + (i + 1));
+                Console.WriteLine("¿cual es su valor ?");
+                int respuesta = aleatorio.Next(100, 400);
+
+                vector[i] = respuesta;
+                Console.WriteLine(vector[i]);
+
+            }
+            Console.WriteLine("escriba el filtro");
+            int filtro = int.Parse(Console.ReadLine());
+            Console.WriteLine("*estas son las posiciones de los objetos que superan el valor minimo*");
+
+            for (int i = 0; i < vector.Count(); i++)
+            {
+                if (vector[i] > filtro)
+                {
+                    Console.WriteLine("° en la posicion " + (i + 1) + " se encuentra un objeto con valor " + vector[i] + " °");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
