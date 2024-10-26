717a50f1975f744b48070aef4dc2e7d9108d3e70
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-26 00:09:26 -0300

    Add files via upload

---
+++ Etapa 2/9_Marque_MiPrimerMatriz/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el el valor de N y M");
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[N, M];
+            for (int c = 0; c < M; c++)
+            {
+
+                for (int f = 0; f < N; f++)
+                {
+                    matriz[f, c] = 0;
+                    Console.Write(matriz[f, c]);
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
