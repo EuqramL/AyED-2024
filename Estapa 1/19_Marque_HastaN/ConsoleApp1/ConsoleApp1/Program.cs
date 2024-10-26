ef2e3ab4126cbc6ba03d7338bdc2ddb657f4b711
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-26 00:23:28 -0300

    TP19

---
+++ Estapa 1/19_Marque_HastaN/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("ingresar valor N");
+            N = int.Parse(Console.ReadLine());
+
+            int numero;
+            numero = 1;
+
+            while (numero < N)
+            {
+                numero++;
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
