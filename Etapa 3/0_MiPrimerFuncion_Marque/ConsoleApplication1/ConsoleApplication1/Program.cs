aa06358e4988ffca8082c8f80aacfff32a2483e9
EuqramL <167466425+EuqramL@users.noreply.github.com>
2024-10-26 00:34:30 -0300

    Add files via upload

---
+++ Etapa 3/0_MiPrimerFuncion_Marque/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string Saludar(string x)
+        {
+            x = "Hola " + x;
+            return x;
+        }
+        static void Main(string[] args)
+        {
+                Console.WriteLine("como te llamas?");
+                string nombre = Console.ReadLine();
+                Console.WriteLine(Saludar(nombre));
+
+                Console.ReadKey();
+            }
+    }
+}
